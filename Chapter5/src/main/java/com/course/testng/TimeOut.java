a80aaa22c8a1cbc323bca87b1fcc135329f4829c
zhangmengmeng1996 <963748383@qq.com>
2019-08-05 18:57:55 +0800

    20190801

--- Chapter5/src/main/java/com/course/testng/TimeOut.java
+++ Chapter5/src/main/java/com/course/testng/TimeOut.java
@@ -3,8 +3,13 @@ package com.course.testng;
 import org.testng.annotations.Test;
 
 public class TimeOut {
+    //超时处理
     @Test(timeOut = 3000)//毫秒
     public void test() throws InterruptedException{
-       // Thread.sleep(millis:2000);
+       Thread.sleep(2000);
+    }
+    @Test(timeOut = 2000)//毫秒
+    public void test2() throws InterruptedException{
+        Thread.sleep(3000);
     }
 }
