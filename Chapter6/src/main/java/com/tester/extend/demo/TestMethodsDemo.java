a33f056250e732aa2fc8546bee7ae9cf0d88152b
zhangmengmeng1996 <963748383@qq.com>
2019-08-06 18:51:16 +0800

    20190806

---
+++ Chapter6/src/main/java/com/tester/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,28 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+    /*
+    断言
+     */
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我自己的日志");
+        //throw new RuntimeException("这是运行时的异常");
+    }
+}
